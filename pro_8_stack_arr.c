9acc1d8d11671abb6c40e2f232a66025ed6023d8
Luqmanhussaini <95037797+Luqmanhussaini@users.noreply.github.com>
2022-03-15 06:49:26 +0530

    Update pro_8_stack_arr.c

--- pro_8_stack_arr.c
+++ pro_8_stack_arr.c
@@ -1,37 +1,72 @@
 #include <stdio.h>
-#define max 20
-int visited[max],a[10][10],n;
-void dfs(int v)
+#include<stdlib.h>
+#define max 5
+int a[max],top=-1;
+void push(int x)
 {
-    printf("%d ",v);
-    visited[v]=1;
-    for(int i=1;i<=n;i++)
+    if(top==max-1)
     {
-        if(a[v][i]==1&& visited[i]==0)
-        {
-            dfs(i);
-        }
+        printf("Stack Overflow\n");
+    }
+    else
+    {
+        a[++top]=x;
     }
 }
-int main()
+int pop()
 {
-    int v;
-    printf("enter the size of the augmented matrix: ");
-    scanf("%d",&n);
-    printf("\n enter the elements in the matrix: \n");
-    for (int i=1;i<=n;i++)
+    if(top==-1)
     {
-        for(int j=1;j<=n;j++)
+        printf("Stack UnderFlow\n");
+        return -1;
+    }
+    else
+    {
+        return a[top--];
+    }
+}
+int peek()
+{
+    return a[top];
+}
+void display()
+{
+    if(top==-1)
+    {
+        printf("there is no elements in the stack");
+    }
+    else
+    {
+        for(int i=0;i<top+1;i++)
         {
-            scanf("%d",&a[i][j]);
+            printf("%d ",a[i]);
+        }
+        printf("\n");
+    }
+}
+void main()
+{
+    int x,ch;
+    while(1)
+    {
+        printf("enter the choice\n1:push\n2:pop\n3:peek\n4:display\n5:exit\n");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1: printf("enter the element you want to push to your stack\n");
+                    scanf("%d",&x);
+                    push(x);
+                    break;
+            case 2: x=pop();
+                    printf("the element %d is popped out\n",x);
+                    break;
+            case 3: x=peek();
+                    printf("tne element %d is at the top of the stack\n",x);
+                    break;
+            case 4: display();
+                    break;
+            case 5: exit(0);
+            default:printf("enter the correct choice\n");
         }
     }
-    printf("enter the v value\n");
-    scanf("%d",&v);
-    for(int i=0;i<n;i++)
-    {
-        visited[i]=0;
-    }
-    dfs(v);
-    return 0;
 }
